5bf8ee707b1967bd6d5bfe4aabf17850a3fafedd
Ivan <hyundai27nn@gmail.com>
2024-09-28 11:19:05 +0200

    + 2

--- js/lesson-01.js
+++ js/lesson-01.js
@@ -1,17 +1,50 @@
 //~ Створи функцію яка приймає число від 0 до 59  і визначає у котру чверть години
 //~ Попадає це число (у першу, другу, третю або четверту).
-function defineQuarter(min) {
-  if (min >= 0 && min <= 15) {
-    return "First quarter";
-  } else if (min > 15 && min <= 30) {
-    return "Second quarter";
-  } else if (min > 30 && min <= 45) {
-    return "Third quarter";
-  } else if (min > 45 && min <= 59) {
-    return "Forth quarter";
-  } else {
-    return "The value of minutes in invalid";
+// function defineQuarter(min) {
+//   if (min >= 0 && min <= 15) {
+//     return "First quarter";
+//   } else if (min > 15 && min <= 30) {
+//     return "Second quarter";
+//   } else if (min > 30 && min <= 45) {
+//     return "Third quarter";
+//   } else if (min > 45 && min <= 59) {
+//     return "Forth quarter";
+//   } else {
+//     return "The value of minutes in invalid";
+//   }
+// }
+
+// console.log(defineQuarter(59));
+
+//~ Напиши функцію яка запитує у користувача ведення числа  1, 2, 3 або 4. Якщо вона має
+//~ значення '1', то у змінну result запишемо 'зима', якщо має значення
+//~ '2' - 'весна' і так далі. Розв'яжіть завдання через switch-case.
+// let valueSeason = prompt("Введіть число від 1 до 4");
+
+function getSeason() {
+  let valueSeason = prompt("Введіть число від 1 до 4");
+  let result = "";
+  switch (Number(valueSeason)) {
+    case 1:
+      result = "Зима";
+      break;
+
+    case 2:
+      result = "Весна";
+      break;
+
+    case 3:
+      result = "Літо";
+      break;
+
+    case 4:
+      result = "Осінь";
+      break;
+
+    default:
+      result = " Вивчи пори року!";
   }
+  return result;
 }
 
-console.log(defineQuarter(59));
+console.log(getSeason());
