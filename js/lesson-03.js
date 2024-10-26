e691433c3d849167398b3db43a719cb2bdd1ea89
Svitlana Markevych <svetlana2507t@gmail.com>
2024-10-26 10:58:54 +0200

    task-2 add

--- js/lesson-03.js
+++ js/lesson-03.js
@@ -98,3 +98,17 @@ function getUserNames(arr) {
 }
 
 console.log(getUserNames(users));
+
+// Отримати масив об'єктів користувачів за кольором очей (поле eyeColor).
+// console.log(getUsersWithEyeColor(users, 'blue')); // [об'єкт Moore Hensley, об'єкт Sharlene Bush, об'єкт Carey Barr]
+//
+// function getUsersWithEyeColor(users, color) {
+//   return users.filter((user) => {
+//     return user.eyeColor === color;
+//   });
+// }
+
+const getUsersWithEyeColor = (users, color) =>
+  users.filter((user) => user.eyeColor === color);
+
+console.log(getUsersWithEyeColor(users, "blue"));
